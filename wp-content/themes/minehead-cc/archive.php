a4b892059ed54e766b4892007af5e6c0c3d612d1
WizardTechie <jez@wizard.technology>
2015-09-18 09:51:39 +0100

    updated archive page

--- wp-content/themes/minehead-cc/archive.php
+++ wp-content/themes/minehead-cc/archive.php
@@ -9,43 +9,58 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+<div class="grid grid-pad">
+	<div class-"col-1-1"><h1>Archive</h1></div>
+	<div class="col-1-4">
+		<?php get_sidebar(); ?>
+	</div>
+	<div class="col-9-12">
+		<div id="primary" class="content-area">
+			<main id="main" class="mcc-news-feed site-main" role="main">
 
-			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
+			<?php if ( have_posts() ) : ?>
 
-		<?php else : ?>
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php endif; ?>
+					<?php
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
+					?>
 
-<?php get_sidebar(); ?>
+				<?php endwhile; ?>
+
+				<?php the_posts_navigation(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+			<?php endif; ?>
+
+
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
+
+</div>
 <?php get_footer(); ?>
